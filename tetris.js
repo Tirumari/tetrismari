b4113806e4af8e0fbf0da840c888b8824186219b
Tirumari Jothi <tirumari@gmail.com>
2021-07-27 09:20:31 -0700

    creates canvas and adds square draw function

--- tetris.js
+++ tetris.js
@@ -9,10 +9,24 @@ const VACANT = '#000000';
 let board = [ ];
 
 for ( let r = 0; r < ROW; r++) {
-    board[r] = [];
+    board[r] = [ ];
     console.log(r)
 
     for( let c = 0; c < COL; c++) {
         board[r][c] = VACANT;
     }
 }
+
+// select canvas
+const cvs = document.getElementById("singleTetris");
+const ctx = cvs.getContext("2d");
+
+// square draw function
+function drawSquare(x, y, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x*SqU, y*SqU, SqU, SqU);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(x*SqU, y*SqU, SqU, SqU);
+};
+
+drawSquare(0,0,'red')
